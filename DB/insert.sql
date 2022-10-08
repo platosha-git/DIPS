521916cd13b280ac01b7b3fdc5a1223de091bbf8
platosha-git <15platosha@gmail.com>
2022-10-08 11:00:52 +0300

    fixed docker

---
+++ DB/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Person (Personid, Firstname, Lastname, Gender, Age)
+VALUES
+(1, 'Richard', 'Williams', 'M', 23),
+(2, 'Marie', 'Simmons', 'F', 31),
+(3, 'Heidi', 'Sanchez', 'M', 59),
+(4, 'Christina', 'Meyer', 'F', 12),
+(5, 'Joseph', 'Diaz', 'M', 7),
+(6, 'Mike', 'Garcia', 'M', 48),
+(7, 'Penny', 'Curry', 'F', 60),
+(8, 'Laura', 'Smith', 'F', 41),
+(9, 'Kenneth', 'Ramsey', 'F', 56),
+(10, 'Albert', 'Owens', 'M', 89);
