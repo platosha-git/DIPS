89495f81eedffc603bb5e66735ba06054892dfb5
platosha-git <15platosha@gmail.com>
2022-10-20 21:51:06 +0300

    fixed configure

--- People/People/Startup.cs
+++ People/People/Startup.cs
@@ -40,7 +40,7 @@ namespace People
 
             // services.AddDbContext<PeopleContext>(option => option.UseNpgsql(config["Connections:Current"]));
             services.AddDbContext<PersonContext>(opt => 
-                opt.UseNpgsql(Configuration.GetConnectionString("Connections:Heroku")));
+                opt.UseNpgsql("User ID=edvkpmgveafcrx;Password=d4d39e7740e84e3b8158af1c62ddecabc990d6d61484ec79efc47a2c4eebca1e;Server=ec2-54-75-184-144.eu-west-1.compute.amazonaws.com;Port=5432;Database=d2cpumffbe0el;"));
             
             // AddDbContext(services, config);
             AddLogging(services, Configuration);
@@ -76,7 +76,7 @@ namespace People
         private static void AddDbContext(IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<PersonContext>(option => 
-                option.UseNpgsql(config.GetConnectionString("Connections:Heroku")));
+                option.UseNpgsql("User ID=edvkpmgveafcrx;Password=d4d39e7740e84e3b8158af1c62ddecabc990d6d61484ec79efc47a2c4eebca1e;Server=ec2-54-75-184-144.eu-west-1.compute.amazonaws.com;Port=5432;Database=d2cpumffbe0el;"));
         }
         
         private static void AddLogging(IServiceCollection services, IConfiguration config)
