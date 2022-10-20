fd74c77177219d3bbc1437e8885430916383031a
platosha-git <15platosha@gmail.com>
2022-10-20 22:00:04 +0300

    fixed app connection string

--- People/People/Startup.cs
+++ People/People/Startup.cs
@@ -40,7 +40,7 @@ namespace People
 
             // services.AddDbContext<PeopleContext>(option => option.UseNpgsql(config["Connections:Current"]));
             services.AddDbContext<PersonContext>(opt => 
-                opt.UseNpgsql("User ID=edvkpmgveafcrx;Password=d4d39e7740e84e3b8158af1c62ddecabc990d6d61484ec79efc47a2c4eebca1e;Server=ec2-54-75-184-144.eu-west-1.compute.amazonaws.com;Port=5432;Database=d2cpumffbe0el;"));
+                opt.UseNpgsql(Configuration.GetConnectionString("Heroku")));
             
             // AddDbContext(services, config);
             AddLogging(services, Configuration);
