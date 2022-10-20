556151e48e30d0f239ce7f92156e6804e33f41e4
platosha-git <15platosha@gmail.com>
2022-10-20 12:46:48 +0300

    fixed database

--- People/People/Migrations/20221020094532_InitialCreate.Designer.cs
+++ People/People/Migrations/20221020094532_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using People.ModelsDB;
 namespace People.Migrations
 {
     [DbContext(typeof(PeopleContext))]
-    [Migration("20221013191628_InitialCreate")]
+    [Migration("20221020094532_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
