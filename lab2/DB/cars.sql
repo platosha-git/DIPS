bfa4dbdb4656ac4323b5ba7f102034762ffc7e4d
platosha-git <15platosha@gmail.com>
2022-11-12 01:00:53 +0300

    done database filling

---
+++ lab2/DB/cars.sql
@@ -0,0 +1,12 @@
+INSERT INTO cars (id, car_uid, brand, model, registration_number, power, price, type, availability)
+VALUES 
+(1, '1997e0e3-d506-4698-b547-00f55ad34d13', 'Kia', 'Rio X', 'Х971ХА', 123, 2100000, 'SUV', False),
+(2, '4c3d90e1-c642-4dac-8b51-85b2c17bc0db', 'Audi', 'Q3', 'Р836УМ', 130, 11600000, 'SEDAN', True),
+(3, 'e64ceb74-f2f3-410e-9f59-17ec11f69ef8', 'BMW', 'X5', 'С252УТ', 125, 23750000, 'SEDAN', True),
+(4, 'db3ab90c-5188-4ff4-a7aa-7e36f1e348c3', 'Ford', 'Evos', 'С105АК', 115, 1170000, 'MINIVAN', False),
+(5, '11345861-9b1b-4228-b7f3-518e6b3f3e7d', 'Ford', 'Maverick', 'Х979РН', 100, 2150000, 'ROADSTER', True),
+(6, 'd0af160c-2631-4833-ae83-f90ad6b274be', 'Honda', 'Civic', 'Т990ТУ', 160, 3450000, 'SUV', True),
+(7, 'b44cf044-f2dc-4548-a9e9-8a99b1476321', 'Kia', 'Sportage', 'Н232ОЕ', 167, 4930000, 'MINIVAN', True),
+(8, '2aaa9773-0ad2-4a41-b1cc-78d8605df4ad', 'Lada', 'Vesta', 'Н329ТС', 90, 800000, 'ROADSTER', False),
+(9, '97a9428d-ec2a-4088-a4f5-380151a1e332', 'Lada', 'Granta', 'Р317АР', 135, 1790000, 'ROADSTER', False),
+(10, '254fe28a-ba4b-465d-817e-edb5ceec9433', 'Mazda', 'CX-5', 'Т104НХ', 103, 3102000, 'SUV', True);
